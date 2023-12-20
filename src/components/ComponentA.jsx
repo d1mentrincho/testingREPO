5801c6e4e86a0c1853bd65c99a434fe2f0069bef
Коваль Дмитро <fyntikminiz@gmail.com>
2023-12-20 15:11:36 +0200

    123123

--- src/components/ComponentA.jsx
+++ src/components/ComponentA.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ComponentA = () => {
     return (
         <div>
-            <h1>Hello, World!123123</h1>
+            <h1>123Hello, World!123123</h1>
         </div>
     );
 };
